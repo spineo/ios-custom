6da63110f761eb4a564da5cf264ab8ebc2d058e9
Stuart Pineo <svpineo@gmail.com>
2017-02-14 09:45:47 -0500

    Custom Collection cells (still coupled with main app)

---
+++ CustomCollectionTableViewCell.h
@@ -0,0 +1,23 @@
+//
+//  CustomCollectionTableViewCell.h
+//  RGButterfly
+//
+//  Created by Stuart Pineo on 7/13/15.
+//  Copyright (c) 2015 Stuart Pineo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+static NSString *CollectionViewCellIdentifier = @"CollectionViewCellIdentifier";
+
+@interface CustomCollectionTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic) CGFloat xOffset;
+
+- (void)setAssocName:(NSString *)desc;
+- (void)setNoLabelLayout;
+
+- (void)setCollectionViewDataSourceDelegate:(id<UICollectionViewDataSource, UICollectionViewDelegate>)dataSourceDelegate index:(NSInteger)index;
+
+@end
